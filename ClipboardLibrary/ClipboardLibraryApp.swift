20c76f4aea5363747da66702d138d6dacd6c7ff7
Praveen Reddy <154207002+Praveen-212@users.noreply.github.com>
2026-09-14 14:50:49 +0530

    Initial Commit

---
+++ ClipboardLibrary/ClipboardLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardLibraryApp.swift
+//  ClipboardLibrary
+//
+//  Created by Praveen reddy on 14/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
